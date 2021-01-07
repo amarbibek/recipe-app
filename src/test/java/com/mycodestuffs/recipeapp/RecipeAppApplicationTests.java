ed641ba9a01bfc17fe2394363d82a15ff0540d40
Bibek <kumar.bee014@gmail.com>
2021-01-07 13:21:53 +0545

    initial commit

---
+++ src/test/java/com/mycodestuffs/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycodestuffs.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
